2b6a8ca15b05a66ea92e51d5f1fec63f7c67b6bf
Shubh Chaturvedi <89243211+ShubhChaturvedi@users.noreply.github.com>
2021-10-08 11:31:10 +0530

    Update simple_intrest.c

--- simple_intrest.c
+++ simple_intrest.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main(){
     int p,r,t,SI;
-    printf("enter the principle amount");
+    printf("enter the principle amount\n");
     scanf("%d",&p);
-    printf("enter the intrest rate amount");
+    printf("enter the intrest rate amount\n");
     scanf("%d",&r);
-    printf("enter the time in years");
+    printf("enter the time in years\n");
     scanf("%d",&t);
     SI=p*r*t/100;
     printf("%d",SI);
